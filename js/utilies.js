3139ef4a47f22d1daef1e4c39aee944fee2ae205
josmar francisco galindo ocampo <addnovel474@gmail.com>
2026-03-20 13:29:29 -0500

    arreglar bugs del boton de correo

--- js/utilies.js
+++ js/utilies.js
@@ -2,11 +2,5 @@ document.getElementById('github').addEventListener('click', () => {
     window.open('https://github.com/MoggerSir', '_blank');
 });
 
-document.getElementById('MandarCorreo').addEventListener('click', () => {
-    const email = 'addnovel1474@gmail.com';
-    const subject = 'Contacto desde Portafolio';
-    const body = 'Hola Josmar, me gustaría contactarte para...';
-    const gmailUrl = `https://mail.google.com/mail/?view=cm&fs=1&to=${email}&su=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
-    window.open(gmailUrl, '_blank');
-});
+// El botón de MandarCorreo usa un enlace mailto nativo en HTML
 
